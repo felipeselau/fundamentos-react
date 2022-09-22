9d597ad04fbe4713569f643f03f20fe7b4f23372
Felipe <86257272+wwfehh@users.noreply.github.com>
2022-09-21 21:34:13 -0300

    update fundamentos-react

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Primeiro(){
+    const msg = 'Seja bem vindo(a)'
+    return (
+    <div>
+        <h2>Primeiro Componente</h2>
+        <p>{ msg} </p>
+        <h6>Este é um componente funcional.</h6>
+    </div>
+    )
+}
+
